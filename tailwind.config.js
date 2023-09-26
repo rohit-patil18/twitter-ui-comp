a9c4f70ca70f6c2e2e9380d209347b7e2794a016
rohit-patil18 <rohit.patil1@somaiya.edu>
2023-09-26 19:09:09 +0530

    declaring variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,47 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "neutral-50": "#F9F9F9",
+        "neutral-100": "#F4F4F4",
+        "neutral-200": "#E4E4E4",
+        "neutral-300": "#D3D3D3",
+        "neutral-400": "#A2A2A2",
+        "neutral-500": "#737373",
+        "neutral-600": "#525252",
+        "neutral-700": "#404040",
+        "neutral-800": "#262626",
+        "neutral-900": "#171717",
+        "neutral-950": "#0A0A0A",
+        "neutral-1000": "#000000",
+        "twitter-blue-default": "#1D9BF0",
+        "twitter-blue-hover": "#1871CA",
+        "twitter-blue-disabled": "#1E5D87",
+        "blue-wash": "rgba(117, 190, 239, 0.2)",
+        "button-stroke": "#546A7A",
+        "searchbar-fill": "#212327",
+        "card-fill": "#16181C",
+        "green-success": "#00BE74",
+        "red-error": "#8B141A",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      padding: {
+        "small-1": "8px 20px",
+        "medium-1": "8px 24px",
+        "large-1": "12px 24px",
+        "small-2": "12px 24px",
+        "medium-2": "15px 93px",
+        "large-2": "20px",
+        "small-3": "6px 16px",
+      },
+      width: {
+        "button-medium": "334px",
+        "button-large": "442px",
+      }
+    },
   },
   plugins: [],
 }
